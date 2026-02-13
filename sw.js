463cb1e107a71ad16ba5e0e9f95ecba2c8eb663c
jackalchiu0930 <62462961+jackalchiu0930@users.noreply.github.com>
2026-02-13 13:59:58 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,30 @@
-const CACHE_NAME = 'jackal-v999'; // 強制刷新
+// --- 版本配置區 (在此修改版本號，啟動畫面將自動同步) ---
+const FE_VERSION = '1.0.0'; 
+const BE_VERSION = '1.0.0';
+const CACHE_NAME = 'jackal-v' + FE_VERSION; // 與前端版本連動
+
 const ASSETS = [
-  './', './Starting.html', './index.html', './test.html', './alerts.html',
-  './01_Starting.jpg', './Bg_JackalAIoT.jpg', './Icon_Jackal.jpg', './manifest.json'
+  './', 
+  './Starting.html', 
+  './index.html', 
+  './test.html', 
+  './alerts.html',
+  './01_Starting.jpg', 
+  './Bg_JackalAIoT.jpg', 
+  './Icon_Jackal.jpg', 
+  './manifest.json'
 ];
 
 self.addEventListener('install', (e) => self.skipWaiting());
+
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => {
+        if (k !== CACHE_NAME) return caches.delete(k);
+      })
+    ))
+  );
 });
 
 self.addEventListener('push', (event) => {
@@ -30,4 +48,3 @@ self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(clients.openWindow(event.notification.data.url));
 });
-
