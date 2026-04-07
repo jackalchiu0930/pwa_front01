a9a30e345bcf7750169d8bd954935a6a6bf7ef7b
jackalchiu0930 <62462961+jackalchiu0930@users.noreply.github.com>
2026-04-07 14:29:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- 版本配置區 ---
-const FE_VERSION = '1.1.3'; // 每次修改請更新版本號
-const BE_VERSION = '1.1.3';
+const FE_VERSION = '1.1.5'; // 每次修改請更新版本號
+const BE_VERSION = '1.1.5';
 const CACHE_NAME = 'jackal-v' + FE_VERSION;
 
 const ASSETS = [
